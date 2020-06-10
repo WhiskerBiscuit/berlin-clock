8ee27d91bf8dbe26038d490df357df5bef45acab
Keenan Lawson <keenanlawson@gmail.com>
2020-06-10 10:55:38 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const getClockTime = (time) => {
+    const clock = {
+        secs: '0',
+        blockHours: '0000',
+        hours: '0000',
+        blockMins: '00000000000',
+        mins: '0000',
+    };
+
+    if (typeof time === 'string') {
+        const [
+            hr,
+            min,
+            sec,
+        ] = time.split(':');
+
+        const seconds = parseInt(sec);
+        const hours = parseInt(hr);
+
+        if (
+            typeof seconds === 'number' &&
+            !isNaN(seconds) &&
+            ((seconds === 0) || !(seconds % 2))
+        ) {
+            clock.secs = 'Y';
+        }
+
+        if (
+            typeof hours === 'number' &&
+            !isNaN(hours)
+        ) {
+            const blockHours = parseInt(hours / 5);
+            const individualHours = hours % 5;
+            const splitBlockHours = clock.blockHours.split('');
+            const splitHours = clock.hours.split('');
+            for (let i = 0; i < splitBlockHours.length; i++) {
+                if (i < blockHours) {
+                    splitBlockHours[i] = 'R';
+                }
+            }
+            for (let i = 0; i < splitHours.length; i++) {
+                if (i < individualHours) {
+                    splitHours[i] = 'R';
+                }
+            }
+            clock.blockHours = splitBlockHours.join('');
+            clock.hours = splitHours.join('');
+        }
+    }
+
+    return `${clock.secs} ${clock.blockHours} ${clock.hours} ${clock.blockMins} ${clock.mins}`;
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [
+        value,
+        setValue,
+    ] = useState('00:00:00');
+
+    // TODO: create UI representation of clock
+    return (
+        <div className="App">
+            <input
+                type="text"
+                value={ value }
+                onChange={ (e) => { setValue(e.target.value); } } />
+            <div>{ getClockTime(value) }</div>
+        </div>
+    );
 }
 
 export default App;
+export {
+    myFunc,
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,70 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { myFunc } from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// it('converts even seconds', () => {
+//   expect(myFunc('00:00:00')).toBe('Y 0000 0000 00000000000 0000');
+//   expect(myFunc('00:00:02')).toBe('Y 0000 0000 00000000000 0000');
+//   expect(myFunc('00:00:04')).toBe('Y 0000 0000 00000000000 0000');
+//   expect(myFunc('00:00:06')).toBe('Y 0000 0000 00000000000 0000');
+//   expect(myFunc('00:00:08')).toBe('Y 0000 0000 00000000000 0000');
+// });
+
+// it('converts odd seconds', () => {
+//     expect(myFunc('00:00:01')).toBe('0 0000 0000 00000000000 0000');
+//     expect(myFunc('00:00:03')).toBe('0 0000 0000 00000000000 0000');
+//     expect(myFunc('00:00:05')).toBe('0 0000 0000 00000000000 0000');
+//     expect(myFunc('00:00:07')).toBe('0 0000 0000 00000000000 0000');
+//     expect(myFunc('00:00:09')).toBe('0 0000 0000 00000000000 0000');
+//   });
+
+// it('converts hours to strange time format', () => {
+//     expect(myFunc('01:00:00')).toBe('Y 0000 R000 00000000000 0000');
+//     expect(myFunc('02:00:00')).toBe('Y 0000 RR00 00000000000 0000');
+//     expect(myFunc('03:00:00')).toBe('Y 0000 RRR0 00000000000 0000');
+//     expect(myFunc('04:00:00')).toBe('Y 0000 RRRR 00000000000 0000');
+//     expect(myFunc('05:00:00')).toBe('Y R000 0000 00000000000 0000');
+//     expect(myFunc('06:00:00')).toBe('Y R000 R000 00000000000 0000');
+//     expect(myFunc('07:00:00')).toBe('Y R000 RR00 00000000000 0000');
+//     expect(myFunc('08:00:00')).toBe('Y R000 RRR0 00000000000 0000');
+//     expect(myFunc('09:00:00')).toBe('Y R000 RRRR 00000000000 0000');
+//     expect(myFunc('10:00:00')).toBe('Y RR00 0000 00000000000 0000');
+//     expect(myFunc('11:00:00')).toBe('Y RR00 R000 00000000000 0000');
+//     expect(myFunc('12:00:00')).toBe('Y RR00 RR00 00000000000 0000');
+//     expect(myFunc('13:00:00')).toBe('Y RR00 RRR0 00000000000 0000');
+//     expect(myFunc('14:00:00')).toBe('Y RR00 RRRR 00000000000 0000');
+//     expect(myFunc('15:00:00')).toBe('Y RRR0 0000 00000000000 0000');
+//     expect(myFunc('16:00:00')).toBe('Y RRR0 R000 00000000000 0000');
+//     expect(myFunc('17:00:00')).toBe('Y RRR0 RR00 00000000000 0000');
+//     expect(myFunc('18:00:00')).toBe('Y RRR0 RRR0 00000000000 0000');
+//     expect(myFunc('19:00:00')).toBe('Y RRR0 RRRR 00000000000 0000');
+//     expect(myFunc('20:00:00')).toBe('Y RRRR 0000 00000000000 0000');
+//     expect(myFunc('21:00:00')).toBe('Y RRRR R000 00000000000 0000');
+//     expect(myFunc('22:00:00')).toBe('Y RRRR RR00 00000000000 0000');
+//     expect(myFunc('23:00:00')).toBe('Y RRRR RRR0 00000000000 0000');
+//     expect(myFunc('00:00:00')).toBe('Y 0000 0000 00000000000 0000');
+// });
+
+it('converts minutes to strange time format', () => {
+    expect(myFunc('00:01:00')).toBe('Y 0000 0000 00000000000 Y000');
+    expect(myFunc('00:02:00')).toBe('Y 0000 0000 00000000000 YY00');
+    expect(myFunc('00:03:00')).toBe('Y 0000 0000 00000000000 YYY0');
+    expect(myFunc('00:04:00')).toBe('Y 0000 0000 00000000000 YYYY');
+    expect(myFunc('00:05:00')).toBe('Y 0000 0000 Y0000000000 0000');
+    expect(myFunc('00:10:00')).toBe('Y 0000 0000 YY000000000 0000');
+    expect(myFunc('00:15:00')).toBe('Y 0000 0000 YYR00000000 0000');
+    expect(myFunc('00:20:00')).toBe('Y 0000 0000 YYRY0000000 0000');
+    expect(myFunc('00:25:00')).toBe('Y 0000 0000 YYRYY000000 0000');
+    expect(myFunc('00:30:00')).toBe('Y 0000 0000 YYRYYR00000 0000');
+    expect(myFunc('00:35:00')).toBe('Y 0000 0000 YYRYYRY0000 0000');
+    expect(myFunc('00:40:00')).toBe('Y 0000 0000 YYRYYRYY000 0000');
+    expect(myFunc('00:45:00')).toBe('Y 0000 0000 YYRYYRYYR00 0000');
+    expect(myFunc('00:50:00')).toBe('Y 0000 0000 YYRYYRYYRY0 0000');
+    expect(myFunc('00:55:00')).toBe('Y 0000 0000 YYRYYRYYRYY 0000');
 });
+
+// it('handles jibberish', () => {
+//     expect(myFunc('jibberish')).toBe('0 0000 0000 00000000000 0000');
+// });
+
+// it('handles characters', () => {
+//     expect(myFunc('00:00:XX')).toBe('0 0000 0000 00000000000 0000');
+// });
